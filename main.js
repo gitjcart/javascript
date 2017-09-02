6193864b82809ee66b9d40c2ca95d018c2cc2921
Josh McCarty <jcart@Joshs-Air.netgear.com>
2017-09-02 12:09:01 -0400

    initial commit for JavaScript files

---
+++ main.js
@@ -0,0 +1,11 @@
+// what do i want my javascript to look like?
+var body=document.getElementsByClassName('skillset');
+function main(){
+$('.body').hide();
+	$('.body').fadeIn(250);
+}
+
+$(document).ready(main);
+
+// alert('you are using the internet!')
+
